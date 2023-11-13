7d54891cd4f4416f83c88722b5e3002e0fb789f2
Liam Sherwin <something.random.i.made.at.3am@gmail.com>
2023-11-13 16:48:40 +1300

    Removed Auth

--- components/layout/navbar.tsx
+++ components/layout/navbar.tsx
@@ -3,18 +3,13 @@
 import Image from "next/image";
 import Link from "next/link";
 import useScroll from "@/lib/hooks/use-scroll";
-import { useSignInModal } from "./sign-in-modal";
-import UserDropdown from "./user-dropdown";
-import { Session } from "next-auth";
 
-export default function NavBar({ session }: { session: Session | null }) {
-  const { SignInModal, setShowSignInModal } = useSignInModal();
+export default function NavBar() {
   const scrolled = useScroll(50);
 
   return (
     <>
-      <SignInModal />
-      <div
+          <div
         className={`fixed top-0 w-full flex justify-center ${
           scrolled
             ? "border-b border-gray-200 bg-white/50 backdrop-blur-xl"
