93475d143136f9050fe42d281053b72312cf7c6b
Peter Gallwas <peter@husky.nz>
2023-12-22 12:08:00 +1300

    Update Screen Shots

--- src/components/Features2.jsx
+++ src/components/Features2.jsx
@@ -18,14 +18,14 @@ export const Features2 = () => (
           <div className="mb-8 lg:mb-0 w-full px-2 lg:pl-16 flex flex-col justify-center md:pl-8">
             <div className=" mb-4 py-3 md:pl-3 md:pr-20 lg:pr-12 rounded">
               <img
-                src="https://raw.githubusercontent.com/SpectrumPro/Spectrum/master/.github/2.png"
+                src="https://serv.husky.nz/spectrum/screenshots/Screen_Shot_2023-12-21_at_10.51.46_PM.png"
                 alt="Dmx software with sliders"
                 className="rounded-xl  custom-border-gray"
               />
             </div>
             <div className="py-3 md:pl-20 lg:pl-12 md:pr-2 rounded ">
               <img
-                src="https://raw.githubusercontent.com/SpectrumPro/Spectrum/dev/.github/Capure.PNG"
+                src="https://serv.husky.nz/spectrum/screenshots/Screen_Shot_2023-12-21_at_10.51.40_PM.png"
                 alt="node based dmx software"
                 className="rounded-xl  custom-border-gray"
               />
